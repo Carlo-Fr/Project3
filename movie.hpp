55b98bbf9120db9e9d77cf9ed0db5092822f771b
Carlo-Fr <148927755+Carlo-Fr@users.noreply.github.com>
2024-12-02 22:37:03 -0500

    Updated movie.hpp with time, filter functions, and basic recommend function
    All sorting functions are now split into mergesort/quicksort versions, and each returns a double of the time it takes to sort. Several filter functions were added that filter the list by year, rating, genre, director, or writer. A recommend function was also added that lists the top 10 recommended (sorted) movies. Sorting functions start line 124, filter functions start line 284, and the recommend function is on line 380

--- movie.hpp
+++ movie.hpp
@@ -1,7 +1,3 @@
-//
-// Created by carlo on 12/1/2024.
-//
-
 #ifndef PROJ3_MOVIE_HPP
 #define PROJ3_MOVIE_HPP
 
@@ -12,6 +8,7 @@
 #include <algorithm>
 #include <unordered_map>
 #include <functional>
+#include <chrono>
 using namespace std;
 
 class Movie {
@@ -54,7 +51,7 @@ public:
         qlist = original;
     }
 
-    // start of mergesort/quicksort functions, jump to line 131 for class functions
+    // start of mergesort/quicksort functions, jump to line 131 for class sort functions, past line 285 are filter functions
 
     void merge(vector<Movie>& list, int left, int mid, int right, function<bool(const Movie&, const Movie&)> comparator) {
         int leftside = mid - left + 1;
@@ -124,21 +121,47 @@ public:
 
     // end of merge/quicksort functions
 
-    void sortbyyear() {
+    double sortbyyearmerge() {
         auto compareyear = [](const Movie& mv1, const Movie& mv2) {
             return mv1.year > mv2.year;
         };
+        auto starttime = chrono::high_resolution_clock::now();
         mergesort(mlist, 0, size - 1, compareyear);
-        quicksort(qlist, 0, size - 1, compareyear);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
     }
-    void sortbyrating() {
+    double sortbyyearquick() {
+        auto compareyear = [](const Movie& mv1, const Movie& mv2) {
+            return mv1.year > mv2.year;
+        };
+        auto starttime = chrono::high_resolution_clock::now();
+        quicksort(qlist, 0, size - 1, compareyear);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
+    }
+    double sortbyratingmerge() {
         auto comparerating = [](const Movie& mv1, const Movie& mv2) {
             return mv1.rating > mv2.rating;
         };
+        auto starttime = chrono::high_resolution_clock::now();
         mergesort(mlist, 0, size - 1, comparerating);
-        quicksort(qlist, 0, size - 1, comparerating);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
     }
-    void sortbygenre(const std::string& inpgenre) {
+    double sortbyratingquick() {
+        auto comparerating = [](const Movie& mv1, const Movie& mv2) {
+            return mv1.rating > mv2.rating;
+        };
+        auto starttime = chrono::high_resolution_clock::now();
+        quicksort(qlist, 0, size - 1, comparerating);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
+    }
+    double sortbygenremerge(const std::string& inpgenre) {
         auto comparegenre = [&inpgenre](const Movie& mv1, const Movie& mv2) -> bool {
             if (mv1.genre.count(inpgenre) && !mv2.genre.count(inpgenre)) {
                 return true;  // mv1 comes first
@@ -148,10 +171,29 @@ public:
                 return false; // Maintain previous order if neither matches
             }
         };
+        auto starttime = chrono::high_resolution_clock::now();
         mergesort(mlist, 0, size - 1, comparegenre);
-        quicksort(qlist, 0, size - 1, comparegenre);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
     }
-    void sortbydirector(const string& drctr) {
+    double sortbygenrequick(const std::string& inpgenre) {
+        auto comparegenre = [&inpgenre](const Movie& mv1, const Movie& mv2) -> bool {
+            if (mv1.genre.count(inpgenre) && !mv2.genre.count(inpgenre)) {
+                return true;  // mv1 comes first
+            } else if (!mv1.genre.count(inpgenre) && mv2.genre.count(inpgenre)) {
+                return false; // mv2 comes first
+            } else {
+                return false; // Maintain previous order if neither matches
+            }
+        };
+        auto starttime = chrono::high_resolution_clock::now();
+        quicksort(qlist, 0, size - 1, comparegenre);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
+    }
+    double sortbydirectormerge(const string& drctr) {
         auto comparedirector = [&drctr](const Movie& mv1, const Movie& mv2) -> bool {
             if (mv1.director.count(drctr) && !mv2.director.count(drctr)) {
                 // mv1 comes first
@@ -166,10 +208,34 @@ public:
                 return false;
             }
         };
+        auto starttime = chrono::high_resolution_clock::now();
         mergesort(mlist, 0, size - 1, comparedirector);
-        quicksort(qlist, 0, size - 1, comparedirector);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
     }
-    void sortbywriter(string& wrtr) {
+    double sortbydirectorquick(const string& drctr) {
+        auto comparedirector = [&drctr](const Movie& mv1, const Movie& mv2) -> bool {
+            if (mv1.director.count(drctr) && !mv2.director.count(drctr)) {
+                // mv1 comes first
+                return true;
+            }
+            else if (!mv1.director.count(drctr) && mv2.director.count(drctr)) {
+                // mv2 comes first
+                return false;
+            }
+            else {
+                // if neither have target, maintain previous order
+                return false;
+            }
+        };
+        auto starttime = chrono::high_resolution_clock::now();
+        quicksort(qlist, 0, size - 1, comparedirector);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
+    }
+    double sortbywritermerge(string& wrtr) {
         auto comparewriter = [&wrtr](const Movie& mv1, const Movie& mv2) {
             if (mv1.writer.count(wrtr) && !mv2.writer.count(wrtr)) {
                 // mv1 comes first
@@ -184,7 +250,153 @@ public:
                 return false;
             }
         };
+        auto starttime = chrono::high_resolution_clock::now();
         mergesort(mlist, 0, size - 1, comparewriter);
-        quicksort(qlist, 0, size - 1, comparewriter);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
     }
+    double sortbywriterquick(string& wrtr) {
+        auto comparewriter = [&wrtr](const Movie& mv1, const Movie& mv2) {
+            if (mv1.writer.count(wrtr) && !mv2.writer.count(wrtr)) {
+                // mv1 comes first
+                return true;
+            }
+            else if (!mv1.writer.count(wrtr) && mv2.writer.count(wrtr)) {
+                // mv2 comes first
+                return false;
+            }
+            else {
+                // if neither have target, maintain previous order
+                return false;
+            }
+        };
+        auto starttime = chrono::high_resolution_clock::now();
+        quicksort(qlist, 0, size - 1, comparewriter);
+        auto endtime = chrono::high_resolution_clock::now();
+        chrono::duration<double> time = endtime - starttime;
+        return time.count();
+    }
+
+    // filter functions
+
+    //filter to specific year
+    void filterbyyear(int yr) {
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            if (it->year != yr) {
+                it = mlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        for (auto it = qlist.begin(); it != qlist.end();) {
+            if (it->year != yr) {
+                it = qlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+    // filter with minimum rating
+    void filterbyrating(double rtng) {
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            if (it->rating < rtng) {
+                it = mlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        for (auto it = qlist.begin(); it != qlist.end();) {
+            if (it->rating < rtng) {
+                it = qlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+    //filter to specific genre
+    void filterbygenre(string& gnre) {
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            if (!it->genre.count(gnre)) {
+                it = mlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        for (auto it = qlist.begin(); it != qlist.end();) {
+            if (!it->genre.count(gnre)) {
+                it = qlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+    //filter to specific director
+    void filterbydirector(string& drctr) {
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            if (!it->director.count(drctr)) {
+                it = mlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        for (auto it = qlist.begin(); it != qlist.end();) {
+            if (!it->director.count(drctr)) {
+                it = qlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+    //filter to specific writer
+    void filterbywriter(string& wrtr) {
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            if (!it->writer.count(wrtr)) {
+                it = mlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+        for (auto it = qlist.begin(); it != qlist.end();) {
+            if (!it->writer.count(wrtr)) {
+                it = qlist.erase(it);
+            }
+            else {
+                it++;
+            }
+        }
+    }
+
+    // Recommend basic function
+    void recommend() {
+        int count = 1;
+        for (auto it = mlist.begin(); it != mlist.end();) {
+            cout << "Recommended Movie " << count << ":" << endl;
+            cout << "Title: " << it->title << endl;
+            cout << "Year: " << it->year << endl;
+            cout << "Rating: " << it->rating << endl;
+            cout << "Genres: ";
+            for (auto it2 = it->genre.begin(); it2 != it->genre.end();) {
+                cout << it2->first << ", ";
+            }
+            cout << endl;
+            cout << "Directors: ";
+            for (auto it3 = it->director.begin(); it3 != it->director.end();) {
+                cout << it3->first << ", ";
+            }
+            cout << endl;
+            cout << endl;
+        }
+    }
+
+
 };
